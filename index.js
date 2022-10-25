3371fb8100a350ed4387785e015f6fefcb73be70
Md Ashik Billah <ashikbillah699@gmail.com>
2022-10-25 21:07:02 +0600

    Courses data load

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require('express');
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
+const courses = require('./data/Courses.json')
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/course_details', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
